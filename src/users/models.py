1f27626901055de44640093393d3540d67a2ae1d
TaeHyoungKwon <kth5604@gmail.com>
2020-01-11 00:39:08 +0900

    3.3 FInishing User model

--- src/users/models.py
+++ src/users/models.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from users.constants import GENDER_CHOICES, LANGUAGE_CHOICES, CURRENCY_CHOICES
+
 
 class User(AbstractUser):
-    GENDER_MALE = "male"
-    GENDER_FEMALE = "female"
-    GENDER_OTHER = "other"
-    GENDER_CHOICES = ((GENDER_MALE, "Male"), (GENDER_FEMALE, "Female"), (GENDER_OTHER, "Other"))
-
     avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True, blank=True)
     bio = models.TextField(default="")
+    birth_date = models.DateField(null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True)
+    is_super_host = models.BooleanField(default=False)
