d3119659440a41fa42a2040d2c9073815c89874b
TaeHyoungKwon <kth5604@gmail.com>
2020-01-11 00:40:58 +0900

    3.4 Falling in Love with Admin panel

--- src/users/admin.py
+++ src/users/admin.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 
 @admin.register(User)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
-
+    list_display = ("username", "email", "gender", "language", "currency", "is_super_host")
+    list_filter = ("language", "currency", "is_super_host")
