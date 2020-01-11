b9a123af603f502a2d3aa94e52542ed6bb84d8b1
TaeHyoungKwon <kth5604@gmail.com>
2020-01-11 19:25:22 +0900

    add room type models

--- src/rooms/models.py
+++ src/rooms/models.py
@@ -5,6 +5,20 @@ from core import models as core_models
 from users import models as user_models
 
 
+class AbstractItem(core_models.TimeStampedModel):
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+    pass
+
+
 class Room(core_models.TimeStampedModel):
     name = models.CharField(max_length=140)
     description = models.TextField()
@@ -20,3 +34,7 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ManyToManyField(RoomType, blank=True)
+
+    def __str__(self):
+        return self.name
