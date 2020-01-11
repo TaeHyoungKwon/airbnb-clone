0b727c36b515347eea9cecc1dae65157d7224c17
TaeHyoungKwon <kth5604@gmail.com>
2020-01-11 19:13:55 +0900

    add room models

--- src/rooms/models.py
+++ src/rooms/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django_countries.fields import CountryField
 
-# Create your models here.
+from src.core.models import TimeStampedModel
+from src.users.models import User
+
+
+class Room(TimeStampedModel):
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    country = CountryField()
+    city = models.CharField(max_length=80)
+    price = models.IntegerField()
+    address = models.CharField(max_length=140)
+    guests = models.IntegerField()
+    beds = models.IntegerField()
+    bedrooms = models.IntegerField()
+    baths = models.IntegerField()
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(User, on_delete=models.CASCADE)
